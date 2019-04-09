e9537eb835d7e02e642705d31d474432615e2025
ZL1Z3R <49368860+ZL1Z3R@users.noreply.github.com>
2019-04-09 15:09:30 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,8 @@
+clear
+figlet -f big "ZL1ZER" | lolcat
+echo "MENGINSTALL BAHAN" | lolcat
+apt update && apt upgrade -y
+pkg install busybox
+echo "TERINSTALL" | lolcat
+clear
+telnet Towel.blinkenlights.nl
